2710f23d2c43e961f581e5f7fab35de98e7aaceb
Jhony Lucas <jhonyjl37@gmail.com>
2024-05-24 00:09:22 -0300

    feat: Register Project, Milestone, Document, and Task models in admin.py

--- project_manager/admin.py
+++ project_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project, Document, Task, Milestone
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Milestone)
+admin.site.register(Document)
+admin.site.register(Task)
